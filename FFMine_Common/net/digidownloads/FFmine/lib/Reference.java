0b314a8c26892a46c78101d33bf3e3e2a07b5355
t999uk <t999uk@digidownloads.net>
2013-06-25 12:10:03 +0100

    tieded up resoruces/strings files

--- FFMine_Common/net/digidownloads/FFmine/lib/Reference.java
+++ FFMine_Common/net/digidownloads/FFmine/lib/Reference.java
@@ -19,48 +19,29 @@ public class Reference {
     public static final String VERSION = "0.0.1";
     
     //Block IDS
-    
-    
-	public static final int oreSilverDragoniteID = 1000;
-
+   /* 
+    public static final int oreSilverDragoniteID = 1000;
 	public static final int oreGoldDragoniteID = 1001;
-
 	public static final int oreRubyDragoniteID = 1002;
-
 	public static final int oreThunderStoneID = 1003;
-
 	public static final int oreWaterStoneID = 1004;
-
 	public static final int oreFireStoneID = 1005;
+	public static final int oreLightStoneID = 1006;*/
 
-	public static final int oreLightStoneID = 1006;
-
+	//item ID's
+	
 	public static final int ShardofDarknessID = 20070;
-
 	public static final int ShardofFireID = 20080;
-
 	public static final int ShardofLightID = 20090;
-
 	public static final int ShardofLightningID = 20100;
-
 	public static final int ShardofWaterID = 20110;
-
 	public static final int ShardofIceID = 20120;
-
 	public static final int ShardofEarthID = 20130;
-
 	public static final int EarthCrystalID = 20140;
-
 	public static final int FireCrystalID = 20150;
-
 	public static final int WaterCrystalID = 20160;
-
 	public static final int ThunderCrystalID = 20170;
-
 	public static final int IceCrystalID = 20180;
-
 	public static final int CrystalofHopeID = 20190;
-
 	public static final int CrystalofDespairID = 20200;
-
 }
--- FFMine_Common/net/digidownloads/FFmine/lib/Strings.java
+++ FFMine_Common/net/digidownloads/FFmine/lib/Strings.java
@@ -8,13 +8,13 @@ public class Strings {
     
     /* Block Name Constants */
     
-    public static final String oreSilverDragonite_Name = "Silver Dragonite";
+    /*public static final String oreSilverDragonite_Name = "Silver Dragonite";
 	public static final String oreGoldDragonite_Name = "Gold Dragonite";
 	public static final String oreRubyDragonite_Name = "Ruby Dragonite";
 	public static final String ThunderStone_Name = "Thunder Stone";
 	public static final String WaterStone_Name = "Water Stone";
 	public static final String FireStone_Name = "Fire Stone";
-	public static final String LightStone_Name = "Light Stone";
+	public static final String LightStone_Name = "Light Stone";*/
 	
 	/* Item Name Constants */
 	
